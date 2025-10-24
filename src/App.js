feb93ee867b88d89ea2a4428da962d41e940db27
p0607 <priyanka1sahani1@gmail.com>
2025-10-24 13:15:00 +0530

    landing page name on tab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
 import LandingPage from './pages/landingpage';
 
 function App() {
+  useEffect(() => {
+    // Set document title as fallback
+    document.title = 'Landing Page';
+  }, []);
+
   return (
     <div>
       <LandingPage />
