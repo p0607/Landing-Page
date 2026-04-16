8fa92612c7f7eaefc872fabe0a2ac0e4b171eaa2
p0607 <priyanka1sahani1@gmail.com>
2026-04-16 11:13:43 +0530

    vms name change

--- src/pages/landingpage.js
+++ src/pages/landingpage.js
@@ -67,7 +67,7 @@ const LandingPage = () => {
       icon: <HandshakeIcon />
     },
     {
-      name: "VMS",
+      name: "Alchemflow [VMS]",
       link: "http://q9lab.in/vms",
       icon: <ServerIcon />
     },
